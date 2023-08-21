7950319f37f50fe1dd9fd9a42f03491479201cd9
Muhammad Haruna Bala <53311062+muhammadHarunaBala@users.noreply.github.com>
2023-08-21 23:06:51 +0100

    final commit

--- lib/utils/enlanguage.dart
+++ lib/utils/enlanguage.dart
@@ -1,19 +1,14 @@
+
 class EnString {
 
 
   static String get login => "Login ";
 
-  static String get addquestion => "Add Question";
-  static String get complatestock => "Complete Stock";
-
-  static String get mostpopularcourses => "Most Popular Courses";
-  static String get coursedetails => "Courses Details";
-
   static String get number => "Mobile Number";
 
   static String get verify => "Verify";
 
-  static String get total => "Total";
+  static String get register => "Register";
 
   static String get FirstName => "First Name";
 
@@ -25,337 +20,190 @@ class EnString {
 
   static String get change => "Change";
 
-  static String get checkout => "Checkout";
+  static String get proceed => "Proceed";
 
   static String get AlreadyHaACC => "Already have account";
 
-  static String get customerservice => "Customer Service";
+  static String get share => "Share";
 
-  static String get startcourseagain => "Start Course Again";
+  static String get downloadc => "Download";
 
-  static String get certificates => "Certificates";
+  static String get pay => "Pay";
 
-  static String get send => "Send";
+  static String get sendtobank => "Send to Bank";
 
-  static String get downloadcertificate => "Download Certificate";
+  static String get sendtonumber => "Send to Number";
 
-  static String get seniorproductmanager => "Senior Product Manager";
+  static String get sendingqr => "Sending with QR";
 
-  static String get productdesigner => "Product Designer";
+  static String get setpasscode => "Set Passcode";
 
-  static String get masteringfigma => "Learn Animation";
+  static String get passcoderequest => "Passcode Request";
 
-  static String get cyndylillibridge => "Cyndy Lillibridge";
+  static String get otp => "OTP";
 
-  static String get vpofmarketing => "VP of Marketing";
+  static String get checkmobile => "Check your mobile messag to see the verification code";
 
-  static String get topmentors => "Top Mentors";
+  static String get incorectnum => "Incorrect number";
 
-  static String get jacobkulikowski => "Jacob Kulikowski";
+  static String get Continue => "Continue";
 
-  static String get marketinganalyst => "Marketing Analyst";
+  static String get acclmt => "Account Limit";
 
-  static String get claireordonez => "Claire Ordonez";
+  static String get accstmt => "Account Statement";
 
-  static String get vpofsales => "VP of Sales";
+  static String get airtimrch => "Airtime Recharge";
 
-  static String get priscillaehrman => "Priscilla Ehrman";
+  static String get electicity => "Electricity";
 
-  static String get uxdesigner => "UX Designer";
+  static String get Internet => "Internet";
 
-  static String get wadechenail => "Wade Chenail";
+  static String get notificatio => "Notification";
 
-  static String get managersolutionengineering =>
-      "Manager, Solution Engineering";
+  static String get paymcomplte => "Payment Completed";
 
-  static String get kathrynpera => "Kathryn Pera";
+  static String get profilesetss => "Profile Settings";
 
-  static String get productmanager => "Product Manager";
+  static String get kycverfi => "KYC Verification";
 
-  static String get evpandgmsalescloud => "EVP and GM, Sales Cloud";
+  static String get donhaveacc => "Don’t have account ?";
 
-  static String get whatsApp => "WhatsApp";
+  static String get sigin => "Sign in";
 
-  static String get wishlist => "Wishlist";
+  static String get aliyu => "Aliyu muhammad";
 
-  static String get website => "Website";
+  static String get requestotp => "Request OTP";
 
-  static String get facebook => "Facebook";
+  static String get balace => " Account balance: ₦ 20,000";
 
-  static String get twitter => "Twitter";
+  static String get messsage => "Messsage";
 
-  static String get instagram => "Instagram";
 
-  static String get yeslogout => "Yes, Logout";
 
-  static String get cancel => "Cancel";
 
-  static String get areyousure => "Are you sure you want to log out?";
 
-  static String get loream =>
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-  static String get profile => "Profile";
 
-  static String get rememberme => "Remember me";
 
-  static String get faceid => "Face ID";
 
-  static String get typesofdata => "1. Types of Data We Collect";
 
-  static String get useofyourpersonaldata => "2. Use of Your Personal Data";
 
-  static String get disclosureofyou => "3. Disclosure of Your Personal Data";
 
-  static String get biometricid => "Biometric ID";
 
-  static String get googleauthenticator => "Google Authenticator";
 
-  static String get enrollcourse => "Enroll Course";
-
-  static String get changepin => "Change PIN";
-
-  static String get changepassword => "Change Password";
-
-  static String get selectthepaymentmethod =>
-      "Select the payment method you want to use";
-
-  static String get paypal => "PayPal";
-
-  static String get select => "Select";
-
-  static String get uidesignbootcamp => "UI Design System";
-
-  static String get generalnotification => "General Notification";
-
-  static String get guyhawkins => "App Design for\nBeginners";
-
-  static String get continues => "Continue";
-
-  static String get yourwishlistishere => "Your 12 wishlist is here!";
-
-  static String get jacobjones => "Jasmin";
-
-  static String get guy => "Kimi";
-
-  static String get forextrading => "All Course";
-
-  static String get connected => "Connected";
-
-  static String get kristinwatson => "Krima";
-
-  static String get photographybecome => "Photography added";
-
-  static String get annetteblack => "juli signh";
-
-  static String get mycourse => "My Course";
-
-  static String get janecooper => "Jane Cooper";
-
-  static String get fitnesstraning => "Basic Fitness Workout";
-
-  static String get comptaeanimation => "Using 3D Animation";
-
-  static String get estherhoward => "Esther Howard";
-
-  static String get complete => "Complete";
-
-  static String get advancetradingforextradingStocktrading =>
-      "Advance TStock Traning";
-
-  static String get sound => "Sound";
-
-  static String get courtneyhenry => "Courtney Henry";
-
-  static String get learnbasicanimation => "Learn Animation";
-
-  static String get devonlane => "Devon Lane";
-
-  static String get vibrate => "Vibrate";
-
-  static String get specialoffers => "Special Offers";
-
-  static String get promoanddiscount => "Promo & Discount";
-
-  static String get payments => "Payments";
-
-  static String get cashback => "Cashback";
-
-  static String get appupdates => "App Updates";
-
-  static String get cardnumber => "Card Number";
-
-  static String get expirydate => "Expiry Date";
-
-  static String get newserviceavailable => "New Service Available";
-
-  static String get cvv => "CVV";
-
-  static String get status => "Status";
-
-  static String get date => "Date";
-
-  static String get transactionid => "Transaction ID";
-
-  static String get paid => "Paid";
-
-  static String get newtipsavailable => "New Tips Available";
-
-  static String get cardname => "Card Name";
-
-  static String get name => "Name";
-
-  static String get enrollcoursesuccessful => "Enroll Course Successful!";
-
-  static String get youhavesuccessfullymade =>
-      "You have successfully made payment and enrolled the course.";
-
-  static String get enteryourpin => "Enter your PIN to confirm payment";
-
-  static String get viewcourse => "View Course";
-
-  static String get update => "Update";
-
-  static String get viewreceipt => "View E-Receipt";
-
-  static String get googlepay => "Google Pay";
-
-  static String get applepay => "Apple Pay";
-
-  static String get addnewcarrd => "Add New Card";
-
-  static String get hellowelcome => "Hello,welcome back to account!";
-
-  static String get email => "Email";
-
-  static String get country => "Country";
-
-  static String get phonenumber => "Phone Number";
-
-  static String get uiuxforbeginner => "Beginner Designer";
-
-  static String get sendcode => "Send Code";
-
-  static String get price => "Price";
-
-  static String get paymentmethods => "Payment Methods";
-
-  static String get unitedstates => "United States";
-
-  static String get creditcard => "Credit Card";
-
-  static String get learnuxuserpersona => "Learn UX Design";
-
-  static String get signinwithgoogle => "Sign in with Google or Facebook";
-
-  static String get ddesing => "3D Design";
-
-  static String get digital => "Digital";
-
-  static String get google => "Google";
-
-  static String get techfigmadesign => "Tech Figma Design";
-
-  static String get phone => "Phone";
-
-  static String get learnbasicui => "Learn UI Basic";
-
-  static String get kyliejenner => "Jenner";
-
-  static String get donthaveanaccount => "Don't have an account?";
-  static String get jastine => "Jastine";
-  static String get margi => "Margi";
-
-  static String get register => "Register";
-
-  static String get enteryoremail => "Enter your email";
-
-  static String get password => "Password";
-
-  static String get enteryourpassword => "Enter your Password";
-
-  static String get verification => "Verification";
-
-  static String get verificationcode => "Verification Code";
-  static String get wehavetosendthecode =>
-      "We have to send the code verification to\nyour email Address";
-
-  static String get sendagain => "Send Again";
-
-  static String get submit => "Submit";
-
-  static String get goodmornin => "Good Morning";
-
-  static String get andrewainsley => "Anika Myara";
-
-  static String get mailid => "Myara@gmail.com";
-
-  static String get editprofile => "Profile Settings";
-
-  static String get notification => "Notification";
-
-  static String get payment => "Payment";
-
-  static String get security => "Security";
-
-  static String get language => "Language";
-
-  static String get kyc => "KYC (Account Upgrade)";
-
-  static String get statement  => " Download statement";
-
-  static String get changepss => " Change Passcode";
-
-  static String get accLmt => "Account Limit";
 
   static String get help => "Help";
 
-  static String get spanish => "Spanish";
+  static String get subject => "Subject";
+  static String get sendmessage => "Send Message";
 
-  static String get french => "French";
-
-  static String get arabic => "Arabic";
-
-  static String get bengali => "Bengali";
-
-  static String get russian => "Russian";
-
-  static String get indonesia => "Indonesia";
-
-  static String get darkmode => "Dark Mode";
-
-  static String get privacypolicy => "Privacy Policy";
-
-  static String get helpcenter => "Help Center";
-
-  static String get faq => "FAQ";
-
-  static String get contactus => "Contact us";
-
-  static String get general => "General";
-
-  static String get account => "Account";
+  static String get call => "Give us a Call ";
+  static String get weareavailable => "We are available all working days from 08:00AM to 05:00PM";
+  static String get contact => "0806819986, 08157665547";
+  static String get emailus => "Email us at";
+  static String get mail => "support@payonetime.ng";
 
 
-  static String get invitefriends => "Invite Friends";
 
-  static String get logout => "Logout";
 
-  static String get search => "Search";
 
-  static String get seeall => "See All";
 
-  static String get mostpopularcourse => "Recommendation";
+  static String get currenlmt => "Current Limit";
 
-  static String get all => "All";
 
-  static String get collection => "Collection";
+  static String get provider => "Service Provider";
+  static String get subpackqge => "Package";
+  static String get mnuber => "Meter Numbert";
+  static String get upgacct => "Upgrade Account";
+  static String get setlmt => "Set a Limit";
 
-  static String get about => "About Us";
+  static String get network => "Network";
+  static String get aboutus => "About Us";
 
-  static String get tools => "Tools";
+  //profile details
 
-  static String get lorem =>
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  static String get name => "Name";
+  static String get email => "Email Address";
+  static String get gender => "Gender";
+  static String get phonenumber => "Phone Number";
+
+  //settin
+
+  static String get kycup => "KYC (Upgrade)";
+  static String get downlodstm => "Download Statement";
+  static String get chagss => "Change Passcode";
+
+
+  static String get addbvn => "Add Your BVN";
+  static String get increslmt => "Increase Limit";
+  static String get upgtobuss => "Upgrade to Business";
+  static String get slcbk => "Select Bank";
+
+
+
+
+
+
+
+
+
+
+
+
+  //static String get register => "Register";
+
+  static String get finalstg => "Final stage";
+
+  static String get createnepssc => "Create New Passcode";
+
+  static String get otpregmssg => "Click request OTP to get OTP message  on your Mobile";
+
+  static String get enterotp => "Enter OTP";
+
+  static String get enternewspsscod => "Enter New Passcode";
+
+  static String get confnewpsscd => "Confirm New Passcode";
+
+  static String get enteramt => "Enter Amount";
+
+  static String get amount => "Enter Amount";
+
+  static String get addcommt => "Add Comment (Optional)";
+
+  static String get tenk => "10,000";
+
+  static String get ngnsign => "₦";
+
+  static String get accnum => " Account Number";
+
+  static String get startdate => "Start Date";
+  static String get endsdate => "End Dtate";
+  static String get date => "12/06/2023";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }

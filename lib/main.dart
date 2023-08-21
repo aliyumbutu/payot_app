34d295b94be67a8c98103e6cecb1dc852a94dadc
Muhammad Haruna Bala <53311062+muhammadHarunaBala@users.noreply.github.com>
2023-08-21 23:08:26 +0100

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:payonetime/screens/home/homescreen.dart';
+import 'package:payonetime/screens/home/paymentcomple.dart';
 import 'package:payonetime/screens/splashscreen/splashscreen.dart';
 import 'package:payonetime/utils/colornotifir.dart';
 import 'package:provider/provider.dart';
@@ -17,8 +19,13 @@ void main() {
           create: (_) => ColorNotifier(),
         ),
       ],
-      child:const MaterialApp(
-        home:Splashscreen(),
+      child: MaterialApp(
+        theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.blueAccent,
+          )
+        ),
+        home:const Splashscreen(),
 
         debugShowCheckedModeBanner: false,
       ),
